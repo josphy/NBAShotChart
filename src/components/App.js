e546fe0aa96aa9a41e53cdb722bc26c8c6c8e032
Haoshan Zou <haoshan@Haoshans-MacBook-Pro.local>
2017-12-21 18:08:36 -0600

    add TopNavBar style

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,12 @@
 import React, { Component } from 'react';
-import '../styles/App.css';
 import {TopNavBar} from "./TopNavBar"
+import '../styles/App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <TopNavBar/>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
