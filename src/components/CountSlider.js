12e1fee57d82bd83b525979e88776806bb434d89
Haoshan Zou <haoshan@Haoshans-MacBook-Pro.local>
2017-12-23 17:30:34 -0600

    add search bar

--- src/components/CountSlider.js
+++ src/components/CountSlider.js
@@ -30,33 +30,3 @@ export class CountSlider extends React.Component {
         );
     }
 }
-
-class DecimalStep extends React.Component {
-    state = {
-        inputValue: 0,
-    }
-    onChange = (value) => {
-        this.setState({
-            inputValue: value,
-        });
-    }
-    render() {
-        return (
-            <Row>
-                <Col span={12}>
-                    <Slider min={0} max={1} onChange={this.onChange} value={this.state.inputValue} step={0.01} />
-                </Col>
-                <Col span={4}>
-                    <InputNumber
-                        min={0}
-                        max={1}
-                        style={{ marginLeft: 16 }}
-                        step={0.01}
-                        value={this.state.inputValue}
-                        onChange={this.onChange}
-                    />
-                </Col>
-            </Row>
-        );
-    }
-}
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import {AutoComplete, Input, Icon} from 'antd';
+import nba from 'nba';
+import {PROFILE_PIC_URL_PREFIX} from "../constants"
 
-function onSelect(value) {
-    console.log('onSelect', value);
-}
+const Option = AutoComplete.Option;
 
 export class SearchBar extends React.Component {
     state = {
@@ -12,24 +12,37 @@ export class SearchBar extends React.Component {
 
     handleSearch = (value) => {
         this.setState({
-            dataSource: !value ? [] : [
-                value,
-                value + value,
-                value + value + value,
-            ],
-        });
+            dataSource: !value ? [] : nba.searchPlayers(value).map((player) => (
+                {
+                    playerId: player.playerId,
+                    fullName: player.fullName,
+                }))
+        })
+    }
+
+    onSelect = (value) => {
+        this.props.handleSelectPlayer(value);
     }
 
     render() {
+        window.nba = nba;
         const {dataSource} = this.state;
+        const options = dataSource.map(player => (
+            <Option key={player.fullName} text={player.fullName} className="player-option">
+                <img className="player-option-image" src={`${PROFILE_PIC_URL_PREFIX}/${player.playerId}.png`} alt="player"/>
+                <span className="player-option-label">{player.fullName}</span>
+            </Option>
+        ));
+
         return (
             <AutoComplete
                 className="search-bar"
                 size="large"
-                dataSource={dataSource}
-                onSelect={onSelect}
+                dataSource={options}
+                onSelect={this.onSelect}
                 onSearch={this.handleSearch}
-                placeholder="input here"
+                placeholder="search NBA player"
+                optionLabelProp="value"
             >
                 <Input suffix={<Icon type="search" className="certain-category-icon"/>}/>
             </AutoComplete>
